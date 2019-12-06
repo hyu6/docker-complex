3be94971d395240ab425b079a162ee48a13f9507
Haoran Yu <haoranyu6@gmail.com>
2019-12-05 20:49:17 -0800

    Troubleshoot startup bugs.

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,11 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
       </div>
     </Router>
   );
